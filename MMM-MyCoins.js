1f871175085c70bb7d31573173be86d2af52181a
THILLET Pierre <pthillet@systra.com>
2020-11-29 12:14:02 +0100

    first commit

---
+++ MMM-MyCoins.js
@@ -0,0 +1,121 @@
+Module.register("MMM-Coinbase", {
+  defaults: {
+    BTCAccount: 0.06555257,
+    totalInvestissement: 800,
+    // updateInterval: 1
+  },
+
+  getStyles: function () {
+    return [this.file("css/styles.css")];
+  },
+
+  start: function () {
+    //Flag for check if module is loaded
+    this.loaded = false;
+    this.BTCEUR = 0;
+    this.BTCUSD = 0;
+    this.BTCAccountPrice = 0;
+    this.balance = 0;
+  },
+
+  getHeader: function () {
+    return (
+      this.data.header +
+      "<span class='right'>" +
+      this.balance +
+      " " +
+      this.currency +
+      "</span>"
+    );
+  },
+
+  getDom: function () {
+    const list = document.createElement("div");
+    list.id = document.createElement("div");
+    const sign = balance >= 0 ? "+" : "-";
+    const colorGain = this.balance >= 0 ? "good" : "bad";
+
+    const DomObj = [
+      {
+        title: "BTC : ",
+        id: "BTCamount",
+        value: this.BTCEUR + " € / " + this.BTCUSD + " $",
+      },
+      {
+        title: "Total de BTC : ",
+        id: "BTCAccount",
+        value: this.config.BTCAccount + " BTC",
+      },
+      {
+        title: "Montant investit : ",
+        id: "totalInvestissement",
+        value: this.config.totalInvestissement + " €",
+      },
+      {
+        title: "Valeur actuelle : ",
+        id: "BTCAccountPrice",
+        value: Math.round(this.BTCAccountPrice) + " €",
+        class: colorGain,
+      },
+      {
+        title: "Gains : ",
+        id: "balance",
+        value: sign + Math.round(this.balance) + " €",
+        class: colorGain,
+      },
+    ];
+
+    DomObj.forEach((div) => {
+      const div = document.createElement("div");
+      const spanTitle = document.createElement("span");
+      spanTitle.innerText = div.title;
+      const spanValue = document.createElement("span");
+      spanValue.id = div.id;
+      if (div.class) spanValue.classList.add(div.class);
+      spanValue.innerText = BTCEUR + " € / " + BTCUSD + " $";
+      div.appendChild(spanTitle);
+      div.appendChild(spanValue);
+      list.appendChild(div);
+    });
+
+    document.getElementById("BTCAccountPrice").classList.add(colorGain);
+    document.getElementById("balance").innerHTML =
+      sign + Math.round(balance) + " €";
+    document.getElementById("balance").classList.add(colorGain);
+
+    const can = document.createElement("canvas");
+    can.id = "myChart";
+    can.width = "350";
+    can.height = "180";
+    list.appendChild(can);
+
+    return list;
+  },
+
+  notificationReceived: function (notification) {
+    switch (notification) {
+      case "DOM_OBJECTS_CREATED":
+        setInterval(() => {
+          this.sendSocketNotification("GET_ACCOUNTS", {
+            apiKey: this.config.apiKey,
+            apiSecret: this.config.apiSecret,
+            wallet: this.config.wallet,
+          });
+        }, 5000);
+
+        break;
+    }
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    switch (notification) {
+      case "ACCOUNTS":
+        this.cryptoData = payload;
+        this.currency = payload[0].native_balance.currency;
+        this.updateDom();
+        this.balance = 0;
+
+        break;
+    }
+  },
+});
